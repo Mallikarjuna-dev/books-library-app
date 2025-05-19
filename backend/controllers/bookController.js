93b06fa0df4352ca29911b81bde3719a8a9d8ef6
Mallikarjuna-dev <76250603+Mallikarjuna-dev@users.noreply.github.com>
2025-05-19 12:34:51 +0530

    updated code

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -5,7 +5,7 @@ const getAllBooks = async (req, res) => {
         const books = await Book.find();
         res.json(books);
     } catch (error) {
-        res.status(500), json({ message: "Failed to fetch books!" });
+        res.status(500).json({ message: "Failed to fetch books!" });
     }
 };
 
